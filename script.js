17e0bacb155ba38952ea2f3becaf31525fe8398d
davidgeorgebell <bell.dave93@gmail.com>
2020-05-04 15:04:46 +0800

    people displayed

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const baseUrl = 'https://hp-api.herokuapp.com/api/characters';
+const peopleGrid = document.querySelector('.people');
 
 const fetchData = async () => {
   const res = await fetch(baseUrl);
-  const data = await res.json();
-  console.log(data);
+  const people = await res.json();
+  displayPeople(people);
 };
 fetchData();
+
+const displayPeople = people => {
+  const html = people.map(
+    person =>
+      `<div class='persons'>
+      <h2 class='name'>${person.name}</h2>
+      </div>`
+  );
+  peopleGrid.innerHTML = html.join('');
+};
