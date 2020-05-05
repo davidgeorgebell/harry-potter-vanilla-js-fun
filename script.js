0adf0998bb26a8b548ed1dea921df121dcf9d897
davidgeorgebell <bell.dave93@gmail.com>
2020-05-05 08:59:40 +0800

    only people with a house are displayed

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 const baseUrl = 'https://hp-api.herokuapp.com/api/characters';
-const peopleGrid = document.querySelector('.people');
+const peopleGrid = document.querySelector('.people-grid');
+
+const houses = [
+  {
+    name: 'Ravenclaw',
+    icon: '🦅',
+    name: 'Slytherin',
+    icon: '🐍',
+    name: 'Gryffindor',
+    icon: '🦁',
+    name: 'Hufflepuff',
+    inon: '🦡',
+  },
+];
 
 const fetchData = async () => {
   const res = await fetch(baseUrl);
   const people = await res.json();
   displayPeople(people);
+  console.log(people);
 };
 fetchData();
 
 const displayPeople = people => {
-  const html = people.map(
-    person =>
-      `<div class='persons'>
-      <h2 class='name'>${person.name}</h2>
-      </div>`
-  );
+  const html = people
+    .filter(person => person.house && person.house)
+    .map(
+      person =>
+        `<div class='person-item'>
+    <h2 class='person-name'>${person.name}</h2>
+    <p class='person-house'>${person.house}</p>
+    </div>`
+    );
   peopleGrid.innerHTML = html.join('');
 };
