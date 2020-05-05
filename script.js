f27b2b8d0035f79ef286b72e5700451affa706e6
davidgeorgebell <bell.dave93@gmail.com>
2020-05-05 10:22:55 +0800

    search functionality

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 const baseUrl = 'https://hp-api.herokuapp.com/api/characters';
 const peopleGrid = document.querySelector('.people-grid');
+const searchBar = document.querySelector('.search-bar');
 
-const houses = [
-  {
-    name: 'Ravenclaw',
-    icon: '🦅',
-    name: 'Slytherin',
-    icon: '🐍',
-    name: 'Gryffindor',
-    icon: '🦁',
-    name: 'Hufflepuff',
-    inon: '🦡',
-  },
+const urls = [
+  `http://hp-api.herokuapp.com/api/characters/students`,
+  `http://hp-api.herokuapp.com/api/characters/staff`,
 ];
 
+let everyone = [];
+
+searchBar.addEventListener('keyup', e => {
+  const searchString = e.target.value;
+  const filteredPeople = everyone.filter(
+    per =>
+      per.name.toLowerCase().includes(searchString) ||
+      per.house.toLowerCase().includes(searchString)
+  );
+  console.log(filteredPeople);
+  displayPeople(filteredPeople);
+});
+
 const fetchData = async () => {
-  const res = await fetch(baseUrl);
-  const people = await res.json();
-  displayPeople(people);
-  console.log(people);
+  try {
+    const people = await Promise.all(
+      urls.map(url => fetch(url).then(res => res.json()))
+    );
+    everyone = people[0].concat(people[1]);
+    return everyone;
+  } catch (error) {
+    console.log(error);
+  }
 };
 fetchData();
 
@@ -29,19 +40,20 @@ const displayPeople = people => {
       person =>
         `<div class='person-item'>
     <h2 class='person-name'>${person.name}</h2>
-    <p class='person-house'>${person.house} ${houseName(person.house)}</p>
+    <p class='person-house'>${person.house}</p>
+    <span class='person-house-icon'>${houseName(person.house)}</span>
     </div>`
     );
   peopleGrid.innerHTML = html.join('');
 };
 const houseName = house => {
   if (house === 'Gryffindor') {
-    return `<span>🦁</span>`;
+    return `<span role='image' aria-label='lion'>🦁</span>`;
   } else if (house === 'Slytherin') {
-    return `<span>🐍</span>`;
+    return `<span role='image' aria-label='snake'>🐍</span>`;
   } else if (house === 'Hufflepuff') {
-    return `<span>🦡</span>`;
+    return `<span role='image' aria-label='lion'>🦡</span>`;
   } else if (house === 'Ravenclaw') {
-    return `<span>🦅</span>`;
+    return `<span role='image' aria-label='lion'>🦅</span>`;
   }
 };
